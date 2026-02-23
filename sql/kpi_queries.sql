aaa324af916991d7290baa1f5b3b274ad2b5be79
danish0406 <danish.nadeem2101@gmail.com>
2026-02-23 11:35:40 +0530

    final touches

--- sql/kpi_queries.sql
+++ sql/kpi_queries.sql
@@ -52,3 +52,14 @@ FROM fact_sales f
 JOIN dim_product dp ON f.product_key = dp.product_key
 GROUP BY dp.product_name
 ORDER BY total_revenue DESC;
+
+
+-- Monthly Revenue Trend
+SELECT 
+    d.year,
+    d.month,
+    SUM(f.revenue) AS total_revenue
+FROM fact_sales f
+JOIN dim_date d ON f.date_key = d.date_key
+GROUP BY d.year, d.month
+ORDER BY d.year, d.month;
