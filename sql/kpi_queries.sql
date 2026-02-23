99efc37b0cd35251b52777540c5a74c7fa9f1831
danish0406 <danish.nadeem2101@gmail.com>
2026-02-23 11:32:25 +0530

    final touches

--- sql/kpi_queries.sql
+++ sql/kpi_queries.sql
@@ -10,3 +10,12 @@ FROM fact_sales;
 SELECT COUNT(*) AS total_orders
 FROM fact_sales;
 
+-- Revenue by Year
+SELECT 
+    d.year,
+    SUM(f.revenue) AS total_revenue
+FROM fact_sales f
+JOIN dim_date d ON f.date_key = d.date_key
+GROUP BY d.year
+ORDER BY d.year;
+
