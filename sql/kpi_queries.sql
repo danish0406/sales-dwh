9eb1a907be5342c0402f8377e7e1a59041303434
danish0406 <danish.nadeem2101@gmail.com>
2026-02-23 11:31:58 +0530

    final touches

---
+++ sql/kpi_queries.sql
@@ -0,0 +1,12 @@
+-- Total Revenue
+SELECT SUM(revenue) AS total_revenue
+FROM fact_sales;
+
+-- Total Orders
+SELECT COUNT(*) AS total_orders
+FROM fact_sales;
+
+-- Total Orders
+SELECT COUNT(*) AS total_orders
+FROM fact_sales;
+
